26c1633c55e663c3be8141a704c369f12e1a4558
zhuyangmsg <370135696@qq.com>
2020-10-10 16:08:02 +0800

    Update index.js

--- src/components/hash-router-dom/index.js
+++ src/components/hash-router-dom/index.js
@@ -3,5 +3,6 @@ import Route from './Route';
 import Link from './Link';
 import Redirect from './Redirect';
 import Switch from './Switch';
+import BrowserRouter from './BrowserRouter';
 
-export {HashRouter,Route,Link,Redirect,Switch}
+export {HashRouter,Route,Link,Redirect,Switch,BrowserRouter}
